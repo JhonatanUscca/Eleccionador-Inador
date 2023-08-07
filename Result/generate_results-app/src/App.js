8c3a339934580340b3c758f58d21e84efaf5210f
JHONATAN BILBAO USCCA GIRALDO <104391558+JhonatanUscca@users.noreply.github.com>
2023-08-06 23:33:34 -0500

    funciones de resultado de las elecciones

--- Result/generate_results-app/src/App.js
+++ Result/generate_results-app/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const ElectionResultsComponent = () => {
+  const [votes, setVotes] = useState([]);
+  const [results, setResults] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleVotesSubmission = (submittedVotes) => {
+    // Verificar si los votos son válidos y consistentes
+    if (!verifyVotes(submittedVotes)) {
+      setErrorMessage('Los votos no son válidos o consistentes.');
+      return;
+    }
+
+    // Procesar los votos y calcular resultados
+    const electionResults = calculateResults(submittedVotes);
+
+    // Verificar si se alcanzó el quórum mínimo
+    if (!hasMinimumQuorum(submittedVotes)) {
+      setErrorMessage('No se alcanzó el quórum mínimo de 20 personas.');
+      return;
+    }
+
+    // Almacenar los votos y resultados
+    setVotes(submittedVotes);
+    setResults(electionResults);
+    setErrorMessage('');
+  };
+
+  const verifyVotes = (submittedVotes) => {
+    // Implementa la lógica de verificación de votos aquí
+    // Retorna true si los votos son válidos, y false si no lo son
+    return true;
+  };
+
+  const calculateResults = (submittedVotes) => {
+    // Implementa la lógica de cálculo de resultados aquí
+    // Retorna los resultados calculados
+    return {};
+  };
+
+  const hasMinimumQuorum = (submittedVotes) => {
+    // Verificar si se alcanzó el quórum mínimo de 20 personas
+    return submittedVotes.length >= 20;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Election Results</h1>
+      <button onClick={() => handleVotesSubmission(/* Pasa los votos ingresados aquí */)}>
+        Procesar Votos
+      </button>
+      {errorMessage && <p>{errorMessage}</p>}
+      {results ? (
+        <div>
+          <h2>Resultados</h2>
+          {/* Muestra los resultados calculados aquí */}
+        </div>
+      ) : (
+        <p>Los resultados no están disponibles.</p>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default ElectionResultsComponent;
