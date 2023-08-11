631770b3788a9a84f2e251cd39cb0660f70b89a3
JHONATAN BILBAO USCCA GIRALDO <104391558+JhonatanUscca@users.noreply.github.com>
2023-08-11 09:39:42 -0500

    actualizaciones

--- Result/generte_ressults/src/App.js
+++ Result/generte_ressults/src/App.js
@@ -1,45 +1,53 @@
-import './App.css';
-import { useState } from "react";
-import Axios from "axios";
+// Importación de estilos y módulos necesarios
+import './App.css';  // Importa el archivo de estilos CSS
+import { useState } from "react";  // Importa la función useState desde React
+import Axios from "axios";  // Importa la librería Axios para hacer solicitudes HTTP
 
+// Definición del componente App
 function App() {
-  const [resultList,setResult] = useState([]);
+  // Estado local para almacenar los resultados de la consulta
+  const [resultList, setResult] = useState([]);
+
+  // Función para obtener y mostrar los resultados
   const addResults = () => {
-      Axios.get("http://localhost:8000/addResults", {
-      }).then((response) => {
-        setResult(response.data)
-      });
+    Axios.get("http://localhost:8000/addResults", {}).then((response) => {
+      setResult(response.data);  // Actualiza el estado con los datos de la respuesta
+    });
   }
+
+  // Renderización del componente
   return (
     <div className="App">
-      <div className="candidateSelection"> 
-        <h2>Resultados</h2>    
+      <div className="candidateSelection">
+        <h2>Resultados</h2>
       </div>
       <div className='lista'>
         <div className='result'>
-        <button className='button' onClick={addResults}>Generara Resultados</button>
-        <h3>El caditato con mas votos:</h3>
-        {
-          resultList.map((val,key)=>{
-            return <div className='contenedor'> {val.name}
-            </div>
+          {/* Botón para generar los resultados */}
+          <button className='button' onClick={addResults}>Generar Resultados</button>
+          <h3>El candidato con más votos:</h3>
+          {
+            // Mapea los resultados y muestra el nombre del candidato con más votos
+            resultList.map((val, key) => {
+              return <div className='contenedor'> {val.name} </div>;
+            })
           }
-          )
-        }
-        <h3>Con la cantida de votos: </h3>
-        {
-          resultList.map((val,key)=>{
-            return <div className='contenedor'> {val.votes}
-            </div>
+          <h3>Con la cantidad de votos:</h3>
+          {
+            // Mapea los resultados y muestra la cantidad de votos del candidato
+            resultList.map((val, key) => {
+              return <div className='contenedor'> {val.votes} </div>;
+            })
           }
-          )
-        }
         </div>
       </div>
       <div>
-      <button className='button'>regesar a la pagina principal</button>
+        {/* Botón para regresar a la página principal */}
+        <button className='button'>Regresar a la página principal</button>
       </div>
     </div>
   );
 }
+
+// Exporta el componente App como predeterminado
 export default App;
--- Result/server/index.js
+++ Result/server/index.js
@@ -1,41 +1,34 @@
-const express = require("express");
-const app = express();
-const mysql = require("mysql");
-const cors = require("cors");
+// Importación de las librerías necesarias
+const express = require("express");  // Importa Express para crear el servidor
+const app = express();  // Crea una instancia de la aplicación Express
+const mysql = require("mysql");  // Importa la librería MySQL para interactuar con la base de datos
+const cors = require("cors");  // Importa CORS para manejar la política de mismo origen
 
-app.use(cors());
-app.use(express.json());
+// Configuración de middlewares
+app.use(cors());  // Habilita CORS para permitir solicitudes desde diferentes dominios
+app.use(express.json());  // Habilita el uso de JSON en las solicitudes y respuestas
 
+// Configuración de la conexión a la base de datos MySQL
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "voting"
+    host: "localhost",  // Host de la base de datos
+    user: "root",  // Usuario de la base de datos
+    password: "",  // Contraseña de la base de datos
+    database: "voting"  // Nombre de la base de datos a utilizar
+});
 
-})
-app.post('/addVote', (req, res) => {
-    const name = req.body.candidate;
-    const instrucSQL = `UPDATE candidate SET votes = votes + 1 WHERE name = '${name}'`;
+// Definición de una ruta GET para obtener los resultados
+app.get('/addResults', (req, res) => {
+    const instrucSQL = `SELECT name, votes FROM candidate ORDER BY votes DESC LIMIT 1`;  // Consulta SQL para obtener el candidato con más votos
     db.query(instrucSQL, (err, resultado) => {
-      if (err) {
-        console.error('Error al actualizar los votos:', err);
-        res.status(500).json({ error: 'Error al actualizar los votos' });
-      } else {
-        res.json({ mensaje: 'Votos actualizados exitosamente' });
-      }
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(resultado);  // Envia los resultados como respuesta
+        }
     });
 });
-app.get('/addResults', (req, res) => {
-  const instrucSQL = `SELECT name, votes FROM candidate ORDER BY votes DESC LIMIT 1`;
-  db.query(instrucSQL, (err, resultado) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(resultado);
-    }
-  });
-});
 
-app.listen(8000,()=>{
-    console.log("corriendo 8000")
-})
+// Inicia el servidor en el puerto 8000
+app.listen(8000, () => {
+    console.log("corriendo 8000");
+});
